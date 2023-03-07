6f72765a2fc3d32fade4dc257643259e2d943ddb
Pabo Bernando <pabobernando69@gmail.com>
2023-03-07 23:24:16 +0700

    xlsx

--- prisma/seed.js
+++ prisma/seed.js
@@ -64,7 +64,7 @@ async function main() {
 
   // Pelatih
   const coach = excelToJson({
-    sourceFile: path.join(__dirname, "seed-data", "dataPelatih.xls"),
+    sourceFile: path.join(__dirname, "seed-data", "dataPelatih.xlsx"),
     header: {
       rows: 1,
     },

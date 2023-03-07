d68b41983fa0f60ecd7225e14c561cd72170f95a
Manda Putra <mandaputra8@gmail.com>
2023-03-07 22:37:05 +0700

    feat: excel seeding (#3)

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,6 @@
+const excelToJson = require("convert-excel-to-json");
+const path = require("path");
 const cabor = require("./seed-data/cabor");
-const atlitAnggar = require("./seed-data/anggar/atlitAnggar");
-// import atlitAnggar from "./anggar/atlitAnggar";
-// import pelatihAnggar from "./anggar/pelatihAnggar";
-// import wasitAnggar from "./anggar/wasitAnggar";
 
 require("dotenv").config();
 
@@ -10,12 +8,58 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+function normalizeAndConcat(obj) {
+  return Object.keys(obj)
+    .map((key) => obj[key])
+    .flat();
+}
+
 async function main() {
   await Promise.all(cabor.map((data) => prisma.cabor.create({ data: data })));
 
-  // Atlit anggar
+  // Atlit
+  const athletes = excelToJson({
+    sourceFile: path.join(__dirname, "seed-data", "dataAtlit.xlsx"),
+    header: {
+      rows: 1,
+    },
+    columnToKey: {
+      A: "nama",
+      B: "alamat",
+      C: "ttl",
+      D: "telephone",
+      E: "gender",
+      F: "cabor_id",
+    },
+  });
+
   await Promise.all(
-    atlitAnggar.map((data) => prisma.atlit.create({ data: data }))
+    normalizeAndConcat(athletes).map((athelete) =>
+      prisma.atlit.create({ data: athelete })
+    )
+  );
+
+  // Wasit
+  const referee = excelToJson({
+    sourceFile: path.join(__dirname, "seed-data", "dataWasit.xlsx"),
+    header: {
+      rows: 1,
+    },
+    columnToKey: {
+      A: "nama",
+      B: "alamat",
+      C: "ttl",
+      D: "telephone",
+      E: "gender",
+      F: "keterangan",
+      G: "cabor_id",
+    },
+  });
+
+  await Promise.all(
+    normalizeAndConcat(referee).map((referee) =>
+      prisma.wasit.create({ data: referee })
+    )
   );
 }
 

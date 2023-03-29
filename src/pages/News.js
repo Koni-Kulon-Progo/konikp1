e853bd8cc32d81c59b0a0d31545e84500a1f3798
Pabo Bernando <pabobernando69@gmail.com>
2023-03-29 22:58:23 +0700

    news carousel

--- src/pages/News.js
+++ src/pages/News.js
@@ -7,9 +7,10 @@ import Koni2 from '../assets/koni2.jpeg'
 import Koni3 from '../assets/koni3.jpeg'
 import Image from 'next/image'
 import { Carousel } from 'antd';
-import Kewr from '../assets/kewr foundation.png'
+import Badminton from '../assets/badminton.png'
 import Koni from '../assets/koni.jpg'
-import MaskotPorda from '../assets/maskotporda.png'
+import Renang from '../assets/renang.png'
+import Karate from '../assets/karate.png'
 
 
 function News() {
@@ -19,13 +20,16 @@ function News() {
   const images = [
     {
       id: 1,
-      url: Kewr
+      url: Karate
     },
     {
       id: 2,
-      url: Koni
+      url: Renang
+    },
+    {
+      id: 3,
+      url: Badminton
     }
-    // ... other images if you want
   ];
 
   return (
@@ -37,18 +41,18 @@ function News() {
     <Carousel autoplay>
   {images.map((image) => (
     <Col key={image.id} span={24} align="center">
-      <Image src={image.url} alt="gambar" width={300} height={200} />
+      <Image src={image.url} alt="gambar" width={150} height={100} />
     </Col>
   ))}
 </Carousel>
             <h1 align="center">News & Event</h1>
-            <h3 align="center">Komite Olahraga Nasional Indonesia KONI Kabupaten Kulon Progo merupakan induk organiasi cabang olahraga yang menaungi sebanyak 45 cabang olahraga (cabor) dan 2 badan olahraga fungsional (Siwo PWI Kulon Progo dan Perwosi Kulon Progo). Pada kepengurusan KONI Kabupaten Kulon Progo masa bakti 2021-2025 yang dipimpin Kusdira BA., segala aktifitas dilakukan di sekretariatan kantor KONI Kulon Progo yang menempati salah satu ruangan di Stadion Cangkring Wates. Untuk kelancaran pelayanan dan administrasi, KONI Kabupaten Kulon Progo mempunyai 2 (dua) orang staf yang bertugas 
+            <h3 align="center" style={{marginBottom: "78px"}}>Komite Olahraga Nasional Indonesia KONI Kabupaten Kulon Progo merupakan induk organiasi cabang olahraga yang menaungi sebanyak 45 cabang olahraga (cabor) dan 2 badan olahraga fungsional (Siwo PWI Kulon Progo dan Perwosi Kulon Progo). Pada kepengurusan KONI Kabupaten Kulon Progo masa bakti 2021-2025 yang dipimpin Kusdira BA., segala aktifitas dilakukan di sekretariatan kantor KONI Kulon Progo yang menempati salah satu ruangan di Stadion Cangkring Wates. Untuk kelancaran pelayanan dan administrasi, KONI Kabupaten Kulon Progo mempunyai 2 (dua) orang staf yang bertugas 
                 setiap hari kerja 5 (lima)</h3>
         </Col>
         <Col span={8} xs={{span:8}} align="center" style={{ backgroundColor: " rgb(0,21,41)"}}>
         <Card
       hoverable
-      style={{ width: 540 }}
+      style={{ width: 340 }}
       cover={
         <Image
           alt="kewr"
@@ -66,7 +70,7 @@ function News() {
         <Col span={8} xs={{span:8}} align="center" style={{ backgroundColor: " rgb(0,21,41)"}}>
         <Card
       hoverable
-      style={{ width: 540 }}
+      style={{ width: 340 }}
       cover={
         <Image
           alt="kewr"
@@ -84,7 +88,7 @@ function News() {
         <Col span={8} xs={{span:8}} align="center" style={{ backgroundColor: " rgb(0,21,41)"}}>
         <Card
       hoverable
-      style={{ width: 540 }}
+      style={{ width: 340 }}
       cover={
         <Image
           alt="kewr"
